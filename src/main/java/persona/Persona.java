0395ab90a116f5efca007267691c33dba3423c74
leozeballez <zeballezleonel@hotmail.com>
2020-04-08 03:07:16 -0300

    error se sube todo

---
+++ src/main/java/persona/Persona.java
@@ -0,0 +1,43 @@
+package persona;
+
+import java.util.Objects;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Persona)) return false;
+        Persona persona = (Persona) o;
+        return Objects.equals(getNombre(), persona.getNombre()) &&
+                Objects.equals(getApellido(), persona.getApellido());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNombre(), getApellido());
+    }
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
---
+++ src/test/java/persona/PersonaTest.java
@@ -0,0 +1,20 @@
+package persona;
+
+public class PersonaTest {
+
+    @org.testng.annotations.Test
+    public void testGetNombre() {
+    }
+
+    @org.testng.annotations.Test
+    public void testGetNombre1() {
+    }
+
+    @org.testng.annotations.Test
+    public void testGetApellido() {
+    }
+
+    @org.testng.annotations.Test
+    public void testGetNombre2() {
+    }
+}
